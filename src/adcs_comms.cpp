2d39904d9881a85b9406f8a9a0e96787cc881f33
g33ya <gscozzaro2004@gmail.com>
2024-11-21 18:05:54 -0500

    nano code

--- src/adcs_comms.cpp
+++ src/adcs_comms.cpp
@@ -1,14 +1,7 @@
 #include "Arduino.h"
 #include "Wire.h"
 #include "SPI.h"
-#include "mbed.h"
-#include "Arduino_PortentaBreakout.h"
 #include <Wire.h>
-
-// === COMMUNICATION ===
-BreakoutCarrierClass Breakout;
-UART myUART = Breakout.UART1;
-
 // === CHECKSUM FUNCTIONS ===
 
 int generate_checksum(int buf[], int len){
@@ -29,24 +22,20 @@ bool verify_checksum(int buf[], int len) {
 
 //--------------------------------------
 
-bool myIncoming = false;
-int myLastUART = -1;
 int totalBytes = 12; // Header + Body bytes, % 4 to find data count value
 
 void setup(){  
-    pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red
- 
-    Serial.begin(115200);    // start serial for output
-    myUART.begin(115200); // baud rate
+    Serial.begin(115200);              
+    Serial1.begin(115200);    // start serial for output
 }
  
 void loop()
 {
   // HEADER (4 bytes)
-  myUART.write(0xC9); // 1st byte: read
-  myUART.write(0x01); // 2nd byte: index
-  myUART.write(0x03); // 3rd byte: data count
-  myUART.write((uint8_t)0x00); // 4th byte: map | error
+  Serial1.write(0xC9); // 1st byte: read
+  Serial1.write(0x01); // 2nd byte: index
+  Serial1.write(0x03); // 3rd byte: data count
+  Serial1.write((uint8_t)0x00); // 4th byte: map | error
 
   // BODY (totalBytes bytes)
   // (Read: returned from ADCS)
@@ -57,46 +46,18 @@ void loop()
   int buf[totalBytes];
   int bytesRead = 0;
   while (bytesRead < totalBytes) {
-      if (myUART.available()) {
-          buf[bytesRead] = myUART.read(); 
+    Serial.write("INFINITE POOP LOOP");
+      if (Serial1.available()) {
+          buf[bytesRead] = Serial1.read(); 
           bytesRead++;
       }
   }
 
-  printf("%d", generate_checksum(buf, totalBytes));        // print the character
-
   // Generate checksum
-  myUART.write(generate_checksum(buf, totalBytes));
-
-
+  Serial.write(generate_checksum(buf, totalBytes));
   //verify_checksum(buf, totalBytes);
 
-  delay(1);
- 
- // DEBUGGING
- /*
- 
-  while(myUART.available()){     // slave may send less than requested
-    //Serial.print("bytes available");        // print the character
-    myIncoming = true;
-    char myChar = myUART.read(); // receive a byte as character (subtracts byte)
-    Serial.print(myChar);        // print the character
-    myLastUART = 2;
-  }
- 
-  if (myIncoming) {  
-      Serial.println();        // just nicely finishes the line.
-      myIncoming = false;
-  }
- 
-  if (myLastUART >=0) {
-     Serial.println("Last Serial message was from UART1:" + String(myLastUART));
-     myLastUART = -1;
-  }
-  */
- 
   Serial.println("Waiting...");
   delay(1000);
-  digitalWrite(LEDB, !digitalRead(LEDB));   //switch back and forth on, off
 }
 
