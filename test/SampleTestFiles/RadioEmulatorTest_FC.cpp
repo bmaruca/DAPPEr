b19c712beb9775cea5652775ab071d23e42dd374
Cj <keleher674@gmail.com>
2025-06-04 13:16:29 -0400

    Created two new files to be used to confirm our Radio Emulator works as intended

---
+++ test/SampleTestFiles/RadioEmulatorTest_FC.cpp
@@ -0,0 +1,83 @@
+#include "Arduino_PortentaBreakout.h"
+
+// SSD segment pins
+breakoutPin A = PWM9;
+breakoutPin B = PWM8;
+breakoutPin C = PWM7;
+breakoutPin D = PWM6;
+breakoutPin E = PWM5;
+breakoutPin Z = PWM4;
+breakoutPin G = PWM3;
+breakoutPin DP = PWM2;
+
+int seg[]{ A, B, C, D, E, Z, G, DP };
+byte Chars[17][7] = {
+  { 1, 1, 1, 1, 1, 1, 0 },  //0
+  { 0, 1, 1, 0, 0, 0, 0 },  //1
+  { 1, 1, 0, 1, 1, 0, 1 },  //2
+  { 1, 1, 1, 1, 0, 0, 1 },  //3
+  { 0, 1, 1, 0, 0, 1, 1 },  //4
+  { 1, 0, 1, 1, 0, 1, 1 },  //5
+  { 1, 0, 1, 1, 1, 1, 1 },  //6
+  { 1, 1, 1, 0, 0, 0, 0 },  //7
+  { 1, 1, 1, 1, 1, 1, 1 },  //8
+  { 1, 1, 1, 1, 0, 1, 1 },  //9
+  { 1, 1, 1, 0, 1, 1, 1 },  //A/10
+  { 0, 0, 1, 1, 1, 1, 1 },  //b/11
+  { 1, 0, 0, 1, 1, 1, 0 },  //C/12
+  { 0, 1, 1, 1, 1, 0, 1 },  //d/13
+  { 1, 0, 0, 1, 1, 1, 1 },  //E/14
+  { 1, 0, 0, 0, 1, 1, 1 },  //F/15
+  { 0, 0, 0, 0, 0, 0, 0 }   //blank
+};
+
+void displayPrint(char n) {
+  for (char i = 0; i < 7; i++) {
+    digitalWrite(seg[i], Chars[n][i]);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);     // Debug
+  Serial3.begin(115200);    // UART3 for RE comms
+
+  // SSD setup
+  for(int i = 0; i < 8; i++) pinMode(seg[i], OUTPUT);
+
+  displayPrint(0);          // Display zero on startup
+}
+
+void loop() {
+  static bool started = false;
+  static unsigned long startMillis = 0;
+
+  // Wait for "START" command from RE
+  if (!started) {
+    if (Serial3.available()) {
+      String cmd = Serial3.readStringUntil('\n');
+      cmd.trim();
+      if (cmd.equalsIgnoreCase("START")) {
+        started = true;
+        startMillis = millis();
+      }
+    }
+    return; // Stay here until started
+  }
+
+  // Cycle 0-F, forever
+  while (true) {
+    for (int i = 0; i < 16; i++) {
+      displayPrint(i);
+
+      // Calculate elapsed time since start of cycling (not power up)
+      unsigned long elapsed = millis() - startMillis;
+
+      // Send digit + elapsed time to RE
+      char buf[40];
+      sprintf(buf, "SSD: %X | t=%lus\n", i, elapsed / 1000);
+      Serial3.print(buf);
+
+      delay(1000); // 1 sec between digits
+    }
+  }
+}
---
+++ test/SampleTestFiles/RadioEmulatorTest_RE.cpp
@@ -0,0 +1,30 @@
+/*This code DOES NOT work in this workspace as it is setup for the Portenta. Copy this code into the Arduino IDE and upload from there
+
+#define FC_BAUD 115200 // Match Portenta's Serial3 baud
+
+void setup() {
+  Serial.begin(115200);    // USB Serial to your PC
+
+  // Specify RX, TX pins for Nano ESP32!
+  Serial1.begin(FC_BAUD, SERIAL_8N1, 44, 43);  // RX=44 (D0), TX=43 (D1)
+
+  delay(100);              // Let everything power up
+  
+  // Send START command to FC
+  Serial1.println("START");
+  Serial.println("Sent 'START' to Flight Computer.");
+}
+
+void loop() {
+  // If data comes from FC over Serial1, relay to PC Serial Monitor
+  if (Serial1.available()) {
+    String line = Serial1.readStringUntil('\n');
+    line.trim();
+    if (line.length() > 0) {
+      Serial.print("FC: ");
+      Serial.println(line);  // e.g. "A,12"
+    }
+  }
+}
+
+*/
